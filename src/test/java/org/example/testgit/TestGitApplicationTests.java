4cc66f652b912d37c75e2bbc2a1f8547157d93b5
ccs <ych74562@gmail.com>
2024-09-18 21:49:05 +0800

    init

---
+++ src/test/java/org/example/testgit/TestGitApplicationTests.java
@@ -0,0 +1,12 @@
+package org.example.testgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
